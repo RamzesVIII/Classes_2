72ce78025a2175fd54b2ab4cfd868313aa6ef647
Roman <litl_rom@tut.by>
2019-10-21 19:12:34 +0300

    5 Task complete

---
+++ User/User/User/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User person = new User("User123", "Alex", "Ivanov", 30);
+            person.Information();
+
+            Console.WriteLine(new string ('-', 50));
+
+            User secondperson = new User("Ivan 12", "Ivan", "Petrov", 25);
+            secondperson.Information();
+            
+        }
+    }
+}
