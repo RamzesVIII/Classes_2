eee847682105b60e5b9232da4907b21d72413653
Roman <litl_rom@tut.by>
2019-12-23 19:51:44 +0300

    salary

---
+++ Employee/Employee/Employee/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    public enum JobPosition
+    {
+        Programmer = 1,
+        Boss = 2,
+        ExtraBoss,
+        Engeneer
+    }
+    public class BaseClass
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public JobPosition Position { get; private set; }
+        
+
+        public BaseClass(string firstName, string lastName, JobPosition position)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+        }
+
+    }
+}
--- Employee/Employee/Employee/Program.cs
+++ Employee/Employee/Employee/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Employee
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Employee enginere = new Employee("Alex", "Ivanov");
-            enginere.salary(enginere);
-        }
+        private Func<double, double, double> fufu = (okl, koe) => { return (okl * koe) + okl; };
+        
+
+            static void Main(string[] args)
+            {
+                Dictionary<JobPosition, Func<double, double, double>> Calkulation = new Dictionary<JobPosition, Func<double, double, double>>();
+                Calkulation.Add(JobPosition.Boss, (okl, koe) => { return (okl * koe) + okl; });
+                Calkulation.Add(JobPosition.ExtraBoss, (okl, koe) => { return (okl * koe) + okl; });
+                Calkulation.Add(JobPosition.Programmer, (okl, koe) => { return (okl * koe) + okl; });
+                Calkulation.Add(JobPosition.Engeneer, (okl, koe) => { return (okl * koe) + okl; });
+
+
+                BaseClass alex = new BaseClass("Alex", "Alexeev", JobPosition.Boss);
+                Calkulation.TryGetValue(JobPosition.Boss, out var func);
+                Console.WriteLine(func(5000,2.3));
+            }
+
+        
     }
 }
